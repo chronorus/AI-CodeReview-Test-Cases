d5df7c713974847cd64f513bac71a150a05b2109
chronorus <chronorus@gmail.com>
2019-09-09 17:04:16 +0700

    Update Case1.java

--- Case1.java
+++ Case1.java
@@ -1,25 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.a.i.codereviewtestcases;
-
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 /**
  *
  * @author john
  */
-public class Case1 {
-    protected static void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ScriptException {
-    String input = req.getParameter("input");  
-    ScriptEngineManager manager = new ScriptEngineManager();
-    ScriptEngine engine = manager.getEngineByName("JavaScript");
-    engine.eval(input); // Noncompliant
-  }
-      
+public class Case1 extends HttpServlet{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String input = req.getParameter("input");
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+        try {
+            engine.eval(input); // Noncompliant
+        } catch (ScriptException ex) {
+            Logger.getLogger(Case1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }      
 }
