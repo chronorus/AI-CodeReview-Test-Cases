ae8c8d2e639ece6d657f977d044bdccf516ed12e
chronorus <chronorus@gmail.com>
2019-09-10 08:10:23 +0700

    Update Case4.java

--- Case4.java
+++ Case4.java
@@ -1,8 +1,14 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class Case4 {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    		String name = req.getParameter("name");
-    		PrintWriter out = resp.getWriter();
-    	out.write("Hello " + name); // Noncompliant
+            String name = req.getParameter("name");
+            PrintWriter out = resp.getWriter();
+            out.write("Hello " + name); // Noncompliant
   	}
 }
   
