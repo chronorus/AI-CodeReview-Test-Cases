e16914765ba385f87981a908ba6db93b68727861
chronorus <chronorus@gmail.com>
2019-09-10 08:22:24 +0700

    Update Case27.java

--- Case27.java
+++ Case27.java
@@ -1,5 +1,6 @@
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.SimpleEmail;
 /*
 Case 27. SMTP SSL connection should check server identity 
@@ -10,11 +11,12 @@ Case 27. SMTP SSL connection should check server identity
  * @author john
  */
 public class Case27 {
-    public void doit() {
+    public void doit() throws EmailException {
         String username = null, password = null;
         Email email = new SimpleEmail();
         email.setSmtpPort(465);
         email.setAuthenticator(new DefaultAuthenticator(username, password));
+        
         email.setSSLOnConnect(true); // Noncompliant; setSSLCheckServerIdentity(true) should also be called before sending the email
         email.send();
     }
